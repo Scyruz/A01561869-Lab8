6c4fb51a7d3e593d2db068c09dbeae44e13964a4
Scyruz <a01561869@itesm.mx>
2021-10-06 17:18:16 -0600

    Challenge 1 and 2

---
+++ src/tools/ProductHelper.ts
@@ -0,0 +1,39 @@
+import Product from "../types/Product";
+
+class ProductHelper {
+  product: Product;
+
+  constructor(product: Product) {
+      this.product = product;
+  }
+
+  getColors() : string[] {
+    let colors = [] as string[];
+
+    if (this.product.childSkus !== undefined) {
+        this.product.childSkus.forEach( (sku) => {
+            if(!colors.includes(sku.color)) {
+              colors.push(sku.color);
+            }
+        });
+    }
+
+    return colors;   
+  }
+
+  getSizes(color : string) : string[] {
+    let sizes = [] as string[];
+
+    if (this.product.childSkus !== undefined) {
+        this.product.childSkus.forEach( (sku) => {
+            if (sku.color === color) {
+                sizes.push(sku.size);
+            }
+        });
+    }
+
+    return sizes;   
+  }
+}
+
+export default ProductHelper;
